55ccd4ec6494ccf749c2c9435c9de51fc2888f0f
Nikhil Raj <nikhil25803@gmail.com>
2024-02-17 03:45:14 +0530

    Added API data in DB

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import VideoDataModel
 
 # Register your models here.
+admin.site.register(VideoDataModel)
--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add", views.get_data, name="query-api"),
+]
